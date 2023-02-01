447f104df98be4891840f342e0b50d96588824a4
malsalih <ali.ali.mohand30@gmail.com>
2023-02-01 11:13:14 +0300

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,7 +180,6 @@ BOOT_SECURITY_PATCH := 2022-11-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Verified Boot
